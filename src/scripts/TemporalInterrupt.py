d1dcd8fd49c33a509649900b22fd048bbdafb9ae
westkath <2509480w@student.gla.ac.uk>
2024-10-26 18:08:36 +0100

    Adjusting for Traces

--- src/scripts/TemporalInterrupt.py
+++ src/scripts/TemporalInterrupt.py
@@ -11,40 +11,33 @@ DEFAULT = "default"
 FILE = "csv"
 DELIMITER = ","
 WORKFLOWS_M = [
-    'mag-orig-ceph-1', 
-    'mag-orig-ceph-2', 
-    'mag-orig-ceph-3',
-    'rangeland-orig-ceph-1', 
-    'rangeland-orig-ceph-2', 
-    'rangeland-orig-ceph-3',
+    'mag-1', 
+    'mag-2', 
+    'mag-3',
+    'rangeland-1', 
+    'rangeland-2', 
+    'rangeland-3'
 ]
 WORKFLOWS_W_N = [
-    'chipseq-orig-ceph-1', 
-    'chipseq-orig-ceph-2', 
-    'chipseq-orig-ceph-3',
-    'chipseq-orig-nfs-1', 
-    'chipseq-orig-nfs-2', 
-    'chipseq-orig-nfs-3',
-    'rnaseq-orig-ceph-1', 
-    'rnaseq-orig-ceph-2', 
-    'rnaseq-orig-ceph-3',
-    'rnaseq-orig-nfs-1', 
-    'rnaseq-orig-nfs-2', 
-    'rnaseq-orig-nfs-3',
-    'sarek-orig-ceph-1', 
-    'sarek-orig-ceph-2', 
-    'sarek-orig-ceph-3',
-    'sarek-orig-nfs-1', 
-    'sarek-orig-nfs-2', 
-    'sarek-orig-nfs-3',
+    'chipseq-1', 
+    'chipseq-2', 
+    'chipseq-3',
+    'rnaseq-1', 
+    'rnaseq-2', 
+    'rnaseq-3',
+    'sarek-1', 
+    'sarek-2', 
+    'sarek-3'
 ]
 WORKFLOWS_W_M = [
-    'montage-orig-ceph-1', 
-    'montage-orig-ceph-2', 
-    'montage-orig-ceph-3',
-    'montage-orig-nfs-1', 
-    'montage-orig-nfs-2', 
-    'montage-orig-nfs-3',
+    'montage-1', 
+    'montage-2', 
+    'montage-3'
+]
+WORKFLOWS_S = [
+    'nanoseq-1',
+    'nanoseq-2',
+    'nanoseq-3'
 ]
 
 
@@ -368,4 +361,4 @@ if __name__ == '__main__':
     max_watts = int(arguments[4])
     ci = parse_ci_intervals(ci_filename)
 
-    main(WORKFLOWS_M, ci, min_watts, max_watts, pue, memory_coefficient)
+    main(WORKFLOWS_S, ci, min_watts, max_watts, pue, memory_coefficient)
