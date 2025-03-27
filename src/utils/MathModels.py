307603a5a5a06ca2072fb93669c1392c88ec626f
Youssef Moawad <youssef_programming@icloud.com>
2025-03-27 17:22:07 +0000

    Adds general polynomial models

--- src/utils/MathModels.py
+++ src/utils/MathModels.py
@@ -1,11 +1,18 @@
-def linear_model(a, b):
-		# Linear model: y = ax + b
-		return lambda x: a * x + b
-
-def quadratic_model(a, b, c):
-		# Quadratic model: y = ax^2 + bx + c
-		return lambda x: a * x**2 + b * x + c
-
-def cubic_model(a, b, c, d):
-		# Cubic model: y = ax^3 + bx^2 + cx + d
-		return lambda x: a * x**3 + b * x**2 + c * x + d
+"""
+coefficients is a list of coefficients for the polynomial model
+The model will be of the form:
+y = a_n * x^n + a_(n-1) * x^(n-1) + ... + a_1 * x + a_0
+where n is the degree of the polynomial and a_i are the coefficients
+"""
+def polynomial_model(coefficients):
+		# Polynomial model: y = a_n * x^n + a_(n-1) * x^(n-1) + ... + a_1 * x + a_0
+		def model(x):
+			result = 0
+			for i, coeff in enumerate(coefficients):
+				result += coeff * (x ** (len(coefficients) - 1 - i))
+			return result
+		return model
+	
+linear_model = lambda a, b: polynomial_model([a, b])
+quadratic_model = lambda a, b, c: polynomial_model([a, b, c])
+cubic_model = lambda a, b, c, d: polynomial_model([a, b, c, d])
