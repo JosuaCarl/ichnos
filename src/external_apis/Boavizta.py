02f5438e9d98a12930dd2ad3b6886b244668b105
Youssef Moawad <youssef_programming@icloud.com>
2025-04-04 15:14:49 +0100

    Adding aws instance impact

--- src/external_apis/Boavizta.py
+++ src/external_apis/Boavizta.py
@@ -1,14 +1,35 @@
-import requests
+from src.utils.APIRequests import make_json_post_request, make_json_get_request
 
 def get_cpu_impact(cpu_name: str):
-    # Construct API URL and headers
+    """
+    Retrieves the global warming potential (GWP) impact value of a CPU by its name.
+    
+    Args:
+        cpu_name (str): The name of the CPU to query (e.g., "Intel Core i7-11700K").
+        
+    Returns:
+        float: The embedded GWP (Global Warming Potential) impact value of the CPU.
+    """
     url = "https://api.boavizta.org/v1/component/cpu?verbose=false"
-    headers = {
-        "accept": "application/json",
-        "Content-Type": "application/json"
-    }
-    payload = { "name": cpu_name }
-    response = requests.post(url, headers=headers, json=payload)
-    return response.json()['impacts']['gwp']['embedded']['value']
+    return make_json_post_request(url, {'name': cpu_name})['impacts']['gwp']['embedded']['value']
+  
+def get_aws_instance_impact(instance_type: str, duration: float = None):
+    """
+    Retrieves the global warming potential (GWP) impact value of an AWS instance type.
+    
+    Args:
+        instance_type (str): The AWS instance type (e.g., "r6g.medium").
+        duration (float, optional): The duration in hours for which to calculate the impact.
+                                   If not provided, returns the total impact value.
+                                   
+    Returns:
+        float: The embedded GWP (Global Warming Potential) impact value of the AWS instance.
+    """
+    url = f"https://api.boavizta.org/v1/cloud/instance?provider=aws&instance_type={instance_type}&verbose=false&criteria=gwp"
+    if duration: url += f"&duration={duration}"
+    return make_json_get_request(url)['impacts']['gwp']['embedded']['value']
 
-print(get_cpu_impact("Intel Core i7-11700K"))
+if __name__ == "__main__":
+	# Example usage
+	print(get_cpu_impact("Intel Core i7-11700K"))
+	print(get_aws_instance_impact("r6g.medium", 24))
